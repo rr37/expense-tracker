241a8d78be35359887a0201153fe98dbd5c257dd
rr37 <ay870527@gmail.com>
2023-05-06 23:32:07 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // 載入express
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 // 建構應用程式伺服器
 const app = express()
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -13,7 +17,7 @@ const PORT = process.env.PORT
 
 // 設定路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定監聽運行訊息
