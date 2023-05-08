b0b0c26e6f8028f009c007b786c6b7d0fff363ba
rr37 <ay870527@gmail.com>
2023-05-08 17:02:27 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 usePassport(app)
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 
 // 設定監聽運行訊息
