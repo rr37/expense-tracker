04a356053e7698b872a1d9e6f7c7a84b950d493a
rr37 <ay870527@gmail.com>
2023-05-08 16:02:08 +0800

    git commit -m "feat: add user login page"

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const router = express.Router()
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
 module.exports = router
+
+router.post('/login', (req, res) => {
+})
+router.get('/register', (req, res) => {
+  res.render('register')
+})
